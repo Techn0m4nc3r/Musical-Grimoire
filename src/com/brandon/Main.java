8a7bac63f8f4150bb66db154416263ab6dd6d365
Brandon <brandon.howar87@gmail.com>
2018-06-29 14:38:30 -0400

    added more functionality

--- src/com/brandon/Main.java
+++ src/com/brandon/Main.java
@@ -5,28 +5,42 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        while (true) {
+        System.out.println("Welcome to Musical Grimoire!\n");
+    //   Ask for input until the user selects quit
+            while (true) {
             Scanner reader = new Scanner(System.in);
-            System.out.println("Enter an album: ");
-
-            String album = reader.nextLine();
-            if (album.equals("1")) {
-                functionone();
-            } else if (album.equals("2")) {
-                functiontwo();
+            menu();
+            String selection = reader.nextLine();
+            if (selection.equals("1")) {
+                addanAlbum();
+            } else if (selection.equals("2")) {
+                chooseanAlbum();
             }
 
             }
+}
+    public static void addanAlbum () {
+        System.out.println("Name of album: ");
+        Scanner reader = new Scanner(System.in);
+        String album = reader.nextLine();
+        System.out.println(album);
+        saveAlbum(album);
 
-
-        }
-
-    public static void functionone () {
-        System.out.println("called function one");
     }
 
-    public static void functiontwo () {
-        System.out.println("called function two");
+    public static void chooseanAlbum () {
+        System.out.println("Choose an album from the list");
+    }
+
+    public static void menu () {
+        System.out.println("What would you like to do?\n");
+        System.out.println("1. Add an album");
+        System.out.println("2. Choose an album");
+        System.out.println("3. Quit");
+    }
+
+    public static void saveAlbum(String album) {
+        System.out.println("Album saved!\n");
     }
 
 }
