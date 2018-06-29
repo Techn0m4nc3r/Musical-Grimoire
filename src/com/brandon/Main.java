a50fe14eb3fc72a789e2ee52be59936c6e7cf605
Brandon <brandon.howar87@gmail.com>
2018-06-29 13:18:34 -0400

    final correction of file mishap

--- src/com/brandon/Main.java
+++ src/com/brandon/Main.java
@@ -1,8 +1,33 @@
 package com.brandon;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        while (true) {
+            Scanner reader = new Scanner(System.in);
+            System.out.println("Enter an album: ");
+
+            String album = reader.nextLine();
+            if (album.equals("1")) {
+                functionone();
+            } else if (album.equals("2")) {
+                functiontwo();
+            }
+
+            }
+
+
+        }
+
+    public static void functionone () {
+        System.out.println("called function one");
     }
+
+    public static void functiontwo () {
+        System.out.println("called function two");
+    }
+
 }
+
