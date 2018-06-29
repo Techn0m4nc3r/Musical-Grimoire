cd130dc08e31fd16075aac7ce29cd73a30340b01
Brandon <brandon.howar87@gmail.com>
2018-06-29 15:22:14 -0400

    added ASCII menu graphic along with the ability to exit upon proper selection

--- src/com/brandon/Main.java
+++ src/com/brandon/Main.java
@@ -16,6 +16,9 @@ public class Main {
             } else if (selection.equals("2")) {
                 chooseanAlbum();
             }
+            else if (selection.equals("3")) {
+                quit();
+            }
 
             }
 }
@@ -32,15 +35,26 @@ public class Main {
         System.out.println("Choose an album from the list");
     }
 
+    public static void quit () {
+        System.out.println("Goodbye!");
+        System.exit(0);
+    }
+
     public static void menu () {
-        System.out.println("What would you like to do?\n");
-        System.out.println("1. Add an album");
-        System.out.println("2. Choose an album");
-        System.out.println("3. Quit");
+        // Menu Graphics
+        System.out.println("============================");
+        System.out.println("|   Music Grimoire         |");
+        System.out.println("============================");
+        System.out.println("|What would you like to do?|");
+        System.out.println("|     1. Add an album      |");
+        System.out.println("|     2. Choose an album   |");
+        System.out.println("|        3. Exit           |");
+        System.out.println("============================");
     }
 
     public static void saveAlbum(String album) {
         System.out.println("Album saved!\n");
+
     }
 
 }
